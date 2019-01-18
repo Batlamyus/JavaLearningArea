e6afcb955accf3aa4017e81751a2d4b51e097f6d
Adil Thinkpad <adil@thinkpad.com>
2019-01-18 22:24:57 +0100

    certification create the tutorial structures

--- src/com/java/certification/datatype/PrimitiveTypes.java
+++ src/com/java/certification/datatype/PrimitiveTypes.java
@@ -1,10 +1,19 @@
 package com.java.certification.datatype;
 
 public class PrimitiveTypes {
-    public static void main(String [] args) {
+
+    public static void printExample(int i){
+        System.out.println("-------Example"+i+"------");
+    }
+    public static void example1(){
+        printExample(1);
         int a = 2;
         boolean res = false;
         res = a++ == 2 || --a == 2 && --a == 2;
-        System.out.println(a);
+        System.out.println(a);//3
+        System.out.println(res);//true
+    }
+    public static void main(String [] args) {
+        example1();
     }
 }
